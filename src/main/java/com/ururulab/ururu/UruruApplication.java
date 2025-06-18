7474d9c9401b54f5917f1959d7ec98c196fed1aa
Joowon-Seo <tjwndnjs1998@gmail.com>
2025-06-18 14:46:00 +0900

    init

---
+++ src/main/java/com/ururulab/ururu/UruruApplication.java
@@ -0,0 +1,13 @@
+package com.ururulab.ururu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UruruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UruruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ururulab/ururu/UruruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ururulab.ururu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UruruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
