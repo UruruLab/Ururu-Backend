33cb3fec184b5884c1e0c29b34c088dc5a159c20
songcw8 <ben990158@gmail.com>
2025-06-19 18:12:03 +0900

    fix: BaseEntity Type 변경및 메서드 추가

--- src/main/java/com/ururulab/ururu/global/common/entity/BaseEntity.java
+++ src/main/java/com/ururulab/ururu/global/common/entity/BaseEntity.java
@@ -2,19 +2,34 @@ package com.ururulab.ururu.global.common.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
+@Getter
 @MappedSuperclass
 public abstract class BaseEntity {
 
     @CreationTimestamp
     @Column(nullable = false, updatable = false)
-    private LocalDateTime createdAt;
+    private ZonedDateTime createdAt; // 생성 시간
 
     @UpdateTimestamp
     @Column(nullable = false)
-    private LocalDateTime updatedAt;
+    private ZonedDateTime updatedAt; // 생성 시간
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = ZonedDateTime.now();
+        this.updatedAt = ZonedDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = ZonedDateTime.now();
+    }
 }
