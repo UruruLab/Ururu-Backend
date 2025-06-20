28d65335e6334cc2690fd0d5219307a6c886b29f
nan0silver <silvernh1220@gmail.com>
2025-06-20 16:12:31 +0900

    feat: Beauty Profile Respository 추가

--- src/main/java/com/ururulab/ururu/user/domain/repository/BeautyProfileRepository.java
+++ src/main/java/com/ururulab/ururu/user/domain/repository/BeautyProfileRepository.java
@@ -1,4 +1,20 @@
 package com.ururulab.ururu.user.domain.repository;
 
-public interface BeautyProfileRepository {
+import com.ururulab.ururu.user.domain.entity.BeautyProfile;
+import com.ururulab.ururu.user.domain.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface BeautyProfileRepository extends JpaRepository<BeautyProfile, Long> {
+    Optional<BeautyProfile> findByMember(Member member);
+    Optional<BeautyProfile> findByMemberId(Long memberId);
+
+    @Query("SELECT bp FROM BeautyProfile bp JOIN FETCH bp.member WHERE bp.member.id = :memberId")
+    Optional<BeautyProfile> findByUserIdWithUser(@Param("memberId") Long memberId);
+
 }
