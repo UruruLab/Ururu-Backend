fc0214f3360153c8e3d1f78103d600ea274cb4f3
23MinL <posing1606@gmail.com>
2025-06-20 23:32:26 +0900

    feat: 카카오 OAuth2 기본 설정 구현

--- src/main/java/com/ururulab/ururu/global/auth/oauth/KakaoOAuthProperties.java
+++ src/main/java/com/ururulab/ururu/global/auth/oauth/KakaoOAuthProperties.java
@@ -1,4 +1,41 @@
 package com.ururulab.ururu.global.auth.oauth;
 
-public class KakaoOAuthProperties {
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * 카카오 OAuth2 설정 프로퍼티.
+ */
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "oauth2.kakao")
+public final class KakaoOAuthProperties {
+
+    private static final String RESPONSE_TYPE = "code";
+    private static final String GRANT_TYPE = "authorization_code";
+
+    private String clientId;
+    private String clientSecret;
+    private String redirectUri;
+    private String authorizationUri;
+    private String tokenUri;
+    private String userInfoUri;
+    private String scope;
+
+    public String buildAuthorizationUrl(final String state) {
+        return String.format(
+                "%s?client_id=%s&redirect_uri=%s&response_type=%s&scope=%s&state=%s",
+                authorizationUri, clientId, redirectUri, RESPONSE_TYPE, scope, state
+        );
+    }
+
+    public String buildTokenRequestBody(final String code) {
+        return String.format(
+                "grant_type=%s&client_id=%s&client_secret=%s&redirect_uri=%s&code=%s",
+                GRANT_TYPE, clientId, clientSecret, redirectUri, code
+        );
+    }
 }
