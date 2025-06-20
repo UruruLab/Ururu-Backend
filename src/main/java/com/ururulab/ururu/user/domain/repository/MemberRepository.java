70dae1eaa8da978c3901ba4215e63c4e0852b960
nan0silver <silvernh1220@gmail.com>
2025-06-20 15:10:03 +0900

    feat: MemberRepository 추가

--- src/main/java/com/ururulab/ururu/user/domain/repository/MemberRepository.java
+++ src/main/java/com/ururulab/ururu/user/domain/repository/MemberRepository.java
@@ -2,15 +2,17 @@ package com.ururulab.ururu.user.domain.repository;
 
 import com.ururulab.ururu.user.domain.entity.Member;
 import com.ururulab.ururu.user.domain.entity.enumerated.SocialProvider;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
     Optional<Member> findBySocialProviderAndSocialId(SocialProvider socialProvider, String socialId);
     Optional<Member> findByNickname(String nickname);
@@ -19,10 +21,19 @@ public interface UserRepository extends JpaRepository<Member, Long> {
     boolean existsByNickname(String nickname);
     boolean existsBySocialProviderAndSocialId(SocialProvider socialProvider, String socialId);
 
-    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN false ELSE true END FROM Member u WHERE u.email = :email AND u.isDeleted = false")
+    @Query("SELECT CASE WHEN COUNT(m) > 0 THEN false ELSE true END FROM Member m WHERE m.email = :email AND m.isDeleted = false")
     boolean isEmailAvailable(@Param("email") String email);
 
-    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN false ELSE true END FROM Member u WHERE u.nickname = :nickname AND u.isDeleted = false")
+    @Query("SELECT CASE WHEN COUNT(m) > 0 THEN false ELSE true END FROM Member m WHERE m.nickname = :nickname AND m.isDeleted = false")
     boolean isNicknameAvailable(@Param("nickname") String nickname);
+
+    @Query("SELECT m FROM Member m WHERE m.id = :memberId AND m.isDeleted = false")
+    Optional<Member> findForWithdrawalPreview(@Param("userId") Long userId);
+
+    @Query("SELECT m FROM Member m WHERE m.isDeleted = false ORDER BY m.updatedAt DESC")
+    List<Member> findRecentActiveUsers(Pageable pageable);
+
+    @Query("SELECT m FROM Member m LEFT JOIN FETCH m.beautyProfile LEFT JOIN FETCH m.shippingAddresses WHERE m.id = :userId AND m.isDeleted = false")
+    Optional<Member> findForDashboard(@Param("memberId") Long userId);
 
 }
