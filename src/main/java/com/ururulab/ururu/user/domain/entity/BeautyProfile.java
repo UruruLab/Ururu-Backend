213794074de78fbf3a41a6211e9f003fe16ea85b
nan0silver <silvernh1220@gmail.com>
2025-06-20 10:13:12 +0900

    feat: BeautyProfile Entity추가

--- src/main/java/com/ururulab/ururu/user/domain/entity/BeautyProfile.java
+++ src/main/java/com/ururulab/ururu/user/domain/entity/BeautyProfile.java
@@ -1,4 +1,62 @@
 package com.ururulab.ururu.user.domain.entity;
 
-public class BeautyProfile {
+import com.ururulab.ururu.global.common.entity.BaseEntity;
+import com.ururulab.ururu.user.domain.entity.enumerated.SkinType;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Table(name = "BeautyProfile")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BeautyProfile extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    private SkinType skinType;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "JSON")
+    private List<String> concerns;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "JSON")
+    private List<String> allergies;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "JSON")
+    private List<String> interestCategories;
+
+    @Column(columnDefinition = "TEXT")
+    private String additionalInfo;
+
+    public static BeautyProfile of(
+            User user,
+            SkinType skinType,
+            List<String> concerns,
+            List<String> allergies,
+            List<String> interestCategories,
+            String additionalInfo
+    ) {
+        BeautyProfile beautyProfile = new BeautyProfile();
+        beautyProfile.user = user;
+        beautyProfile.skinType = skinType;
+        beautyProfile.concerns = concerns;
+        beautyProfile.allergies = allergies;
+        beautyProfile.interestCategories = interestCategories;
+        beautyProfile.additionalInfo = additionalInfo;
+        return beautyProfile;
+    }
 }
