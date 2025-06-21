fbddc4d073b13584dd4f5e6f713423efc2b8d3cd
songcw8 <ben990158@gmail.com>
2025-06-21 16:10:17 +0900

    feat: ProductOptionRepository 생성

--- src/main/java/com/ururulab/ururu/product/domain/repository/ProductOptionRepository.java
+++ src/main/java/com/ururulab/ururu/product/domain/repository/ProductOptionRepository.java
@@ -3,6 +3,10 @@ package com.ururulab.ururu.product.domain.repository;
 import com.ururulab.ururu.product.domain.entity.ProductOption;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductOptionRepository extends JpaRepository<ProductOption, Long> {
+import java.util.List;
+import java.util.Optional;
 
+public interface ProductOptionRepository extends JpaRepository<ProductOption, Long> {
+    // 특정 상품의 모든 옵션 조회 (삭제되지 않은 것만)
+    List<ProductOption> findByProductIdAndIsDeletedFalse(Long productId);
 }
