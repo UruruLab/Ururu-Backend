7b432b1dfb02ca437fdab951bee74650fd5ba65b
Joowon-Seo <tjwndnjs1998@gmail.com>
2025-06-21 19:27:43 +0900

    feat: ReviewImage 개수 검증 로직 구현

---
+++ src/main/java/com/ururulab/ururu/review/domain/policy/ReviewImagePolicy.java
@@ -0,0 +1,8 @@
+package com.ururulab.ururu.review.domain.policy;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ReviewImagePolicy {
+	public final int MAX_IMAGE_COUNT = 5;
+}
