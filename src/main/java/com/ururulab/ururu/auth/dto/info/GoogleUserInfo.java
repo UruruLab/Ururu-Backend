6666e2a8faf10d65d60d55c5526a6def06015c7c
23MinL <posing1606@gmail.com>
2025-06-27 03:18:32 +0900

    feat: Google 사용자 정보 응답 DTO 구현

--- src/main/java/com/ururulab/ururu/auth/dto/info/GoogleUserInfo.java
+++ src/main/java/com/ururulab/ururu/auth/dto/info/GoogleUserInfo.java
@@ -1,4 +1,29 @@
 package com.ururulab.ururu.auth.dto.info;
 
-public class GoogleUserInfo {
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record GoogleUserInfo(
+        String id,
+        String email,
+        @JsonProperty("verified_email") boolean verifiedEmail,
+        String name,
+        @JsonProperty("given_name") String givenName,
+        @JsonProperty("family_name") String familyName,
+        String picture,
+        String locale
+) {
+
+    public static GoogleUserInfo of(
+            final String id,
+            final String email,
+            final boolean verifiedEmail,
+            final String name,
+            final String givenName,
+            final String familyName,
+            final String picture,
+            final String locale
+    ) {
+        return new GoogleUserInfo(id, email, verifiedEmail, name,
+                givenName, familyName, picture, locale);
+    }
 }
