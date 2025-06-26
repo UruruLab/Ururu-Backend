aa1f0c541b7cb868afcfee40cd1375b25da02428
23MinL <posing1606@gmail.com>
2025-06-27 03:53:50 +0900

    refactor: @EnableConfigurationProperties 어노테이션 제거

--- src/main/java/com/ururulab/ururu/UruruApplication.java
+++ src/main/java/com/ururulab/ururu/UruruApplication.java
@@ -8,7 +8,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @SpringBootApplication
-@EnableConfigurationProperties(JwtProperties.class)
 public class UruruApplication {
 
 	public static void main(String[] args) {
