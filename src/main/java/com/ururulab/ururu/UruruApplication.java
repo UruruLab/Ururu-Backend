a268c1bd5360b40a3c89ce4867290a71a1235a47
23MinL <posing1606@gmail.com>
2025-06-26 13:28:18 +0900

    feat: JwtProperties 바인딩을 위한 @EnableConfigurationProperties(JwtProperties.class) 추가

--- src/main/java/com/ururulab/ururu/UruruApplication.java
+++ src/main/java/com/ururulab/ururu/UruruApplication.java
@@ -1,11 +1,14 @@
 package com.ururulab.ururu;
 
+import com.ururulab.ururu.auth.jwt.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @SpringBootApplication
+@EnableConfigurationProperties(JwtProperties.class)
 public class UruruApplication {
 
 	public static void main(String[] args) {
