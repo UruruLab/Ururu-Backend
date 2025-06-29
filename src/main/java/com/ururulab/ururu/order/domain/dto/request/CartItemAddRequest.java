6d29b8b33ae59e116f903c44a30fd0304a82473c
taehyun <kimtaehyun002@gmail.com>
2025-06-29 16:20:48 +0900

    refactor: 장바구니/주문 validation 분리

--- src/main/java/com/ururulab/ururu/order/domain/dto/request/CartItemAddRequest.java
+++ src/main/java/com/ururulab/ururu/order/domain/dto/request/CartItemAddRequest.java
@@ -1,18 +1,18 @@
 package com.ururulab.ururu.order.domain.dto.request;
 
-import com.ururulab.ururu.order.domain.dto.validation.ValidationConstants;
-import com.ururulab.ururu.order.domain.dto.validation.ValidationMessages;
+import com.ururulab.ururu.order.domain.dto.validation.CartValidationConstants;
+import com.ururulab.ururu.order.domain.dto.validation.CartValidationMessages;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 public record CartItemAddRequest(
-        @NotNull(message = ValidationMessages.GROUPBUY_OPTION_ID_REQUIRED)
+        @NotNull(message = CartValidationMessages.GROUPBUY_OPTION_ID_REQUIRED)
         Long groupbuyOptionId,
 
-        @NotNull(message = ValidationMessages.QUANTITY_REQUIRED)
-        @Min(value = ValidationConstants.QUANTITY_MIN, message = ValidationMessages.QUANTITY_MIN)
-        @Max(value = ValidationConstants.QUANTITY_MAX, message = ValidationMessages.QUANTITY_MAX)
+        @NotNull(message = CartValidationMessages.QUANTITY_REQUIRED)
+        @Min(value = CartValidationConstants.QUANTITY_MIN, message = CartValidationMessages.QUANTITY_MIN)
+        @Max(value = CartValidationConstants.QUANTITY_MAX, message = CartValidationMessages.QUANTITY_MAX)
         Integer quantity
 ) {
 }
