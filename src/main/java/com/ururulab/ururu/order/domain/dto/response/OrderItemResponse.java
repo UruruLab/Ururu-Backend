1f2482a6de8849fc355745f8f23f0c578ff56770
taehyun <kimtaehyun002@gmail.com>
2025-06-29 18:17:33 +0900

    feat: OrderItemResponse DTO 생성

---
+++ src/main/java/com/ururulab/ururu/order/domain/dto/response/OrderItemResponse.java
@@ -0,0 +1,13 @@
+package com.ururulab.ururu.order.domain.dto.response;
+
+/**
+ * 주문 아이템 정보 응답 DTO
+ */
+public record OrderItemResponse(
+        Long groupbuyOptionId,
+        Integer quantity,
+        String productName,
+        String optionName,
+        Integer price
+) {
+}
