2fe175a740551508bf82b23d73c509bb573b7ca4
taehyun <kimtaehyun002@gmail.com>
2025-06-29 16:13:37 +0900

    feat: OrderStatus에 PENDING 상태 추가

--- src/main/java/com/ururulab/ururu/order/domain/entity/enumerated/OrderStatus.java
+++ src/main/java/com/ururulab/ururu/order/domain/entity/enumerated/OrderStatus.java
@@ -3,7 +3,8 @@ package com.ururulab.ururu.order.domain.entity.enumerated;
 import com.ururulab.ururu.global.domain.entity.enumerated.EnumParser;
 
 public enum OrderStatus {
-    ORDERED,    // 주문 완료
+    PENDING,    // 주문서 생성 (재고 예약, 결제 대기)
+    ORDERED,    // 주문 완료 (결제 완료, 재고 확정)
     CANCELLED,  // 주문 취소
     REFUNDED;   // 환불 완료
 
