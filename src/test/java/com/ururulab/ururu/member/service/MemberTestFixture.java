88c7e7e2290b13766c2782948472825d4c972404
nan0silver <silvernh1220@gmail.com>
2025-07-12 04:05:16 +0900

    refactor: 프로필 수정 API 관련 서비스 로직 Test code 수정

--- src/test/java/com/ururulab/ururu/member/service/MemberTestFixture.java
+++ src/test/java/com/ururulab/ururu/member/service/MemberTestFixture.java
@@ -5,7 +5,7 @@ import com.ururulab.ururu.global.domain.entity.enumerated.Gender;
 import com.ururulab.ururu.member.domain.entity.Member;
 import com.ururulab.ururu.member.domain.entity.enumerated.Role;
 import com.ururulab.ururu.member.domain.entity.enumerated.SocialProvider;
-import com.ururulab.ururu.member.dto.request.MemberRequest;
+import com.ururulab.ururu.member.dto.request.MemberUpdateRequest;
 
 import java.lang.reflect.Field;
 import java.time.Instant;
@@ -38,19 +38,14 @@ public class MemberTestFixture {
         return member;
     }
 
-    public static MemberRequest createMemberUpdateRequest(String nickname, String phone) {
-        return new MemberRequest(
+    public static MemberUpdateRequest createMemberUpdateRequest(String nickname, String phone) {
+        return new MemberUpdateRequest(
                 nickname,
-                "updated@example.com",
-                SocialProvider.GOOGLE,
-                "social123",
                 "MALE",
                 Instant.parse("1985-01-01T00:00:00Z"),
-                phone,
-                null
+                phone
         );
     }
-
 
 
     private static void setMemberId(Member member, Long id){
