8052e884fb18cad36aa338403cfa70aeece5eca9
taehyun <kimtaehyun002@gmail.com>
2025-07-13 16:31:40 +0900

    feat: 환불 요청 DTO 반송 운송장 필드 추가

--- src/main/java/com/ururulab/ururu/payment/dto/request/RefundRequestDto.java
+++ src/main/java/com/ururulab/ururu/payment/dto/request/RefundRequestDto.java
@@ -14,6 +14,9 @@ public record RefundRequestDto(
 
         @NotBlank(message = RefundValidationMessages.REASON_REQUIRED)
         @Size(max = RefundValidationConstants.REASON_MAX_LENGTH, message = RefundValidationMessages.REASON_MAX_LENGTH)
-        String reason
+        String reason,
+
+        @Size(max = RefundValidationConstants.RETURN_TRACKING_NUMBER_MAX_LENGTH, message = RefundValidationMessages.RETURN_TRACKING_NUMBER_TOO_LONG)
+        String returnTrackingNumber  //
 ) {
 }
