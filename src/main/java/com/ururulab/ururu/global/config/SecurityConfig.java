70538321acda3f6b02609e1b86caa2ea6f6cec68
1000hyehyang <ducogus12@gmail.com>
2025-08-24 19:07:30 +0900

    feat: HTTP 상태 코드 일관성 개선 및 Spring Security 예외 처리 강화
    - 인증 관련 에러 코드를 401 Unauthorized로 통일
    - Spring Security AccessDeniedException, AuthenticationException 처리 추가
    - JwtAuthenticationFilter에서 더 명확한 에러 로깅 구현
    - TokenValidator에 상세한 디버그 로그 추가
    - AuthUtils에서 권한 검증 로직 개선
    - SecurityConfig에 @EnableMethodSecurity 활성화

--- src/main/java/com/ururulab/ururu/global/config/SecurityConfig.java
+++ src/main/java/com/ururulab/ururu/global/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -19,6 +20,7 @@ import org.springframework.http.HttpMethod;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
